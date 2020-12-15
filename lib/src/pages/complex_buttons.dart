f2ea5c53ed76ddad1e936e0aaee0c7d8fba5eb21
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-15 17:35:47 +0100

    [FEATURE][GRID SCREEN] * Header title was added

--- lib/src/pages/complex_buttons.dart
+++ lib/src/pages/complex_buttons.dart
@@ -7,7 +7,16 @@ class ComplexButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [_backgroundGradient()],
+        children: [
+          _backgroundGradient(),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                _headerTitle(),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -33,24 +42,40 @@ class ComplexButtons extends StatelessWidget {
           height: 360,
           width: 360,
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(236, 98, 188, 1),
-                Color.fromRGBO(241, 142, 172, 1),
-              ],
-            ),
-              color: Colors.pink, borderRadius: BorderRadius.circular(85)),
-        )
-    );
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(236, 98, 188, 1),
+                  Color.fromRGBO(241, 142, 172, 1),
+                ],
+              ),
+              color: Colors.pink,
+              borderRadius: BorderRadius.circular(85)),
+        ));
 
     return Stack(
       children: [
         _linearGradient,
         Positioned(
-            child: _pinkBox,
+          child: _pinkBox,
           top: -100,
         ),
       ],
+    );
+  }
+
+  Widget _headerTitle() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Classify transaction', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),),
+            SizedBox(height: 10,),
+            Text('Classify this transaction into a particular category', style: TextStyle(color: Colors.white, fontSize: 18),),
+          ],
+        ),
+      ),
     );
   }
 }
